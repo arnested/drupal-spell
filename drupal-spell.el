e9621252abe40df073b92408c6b9c0d63a8dcd5e
Arne Jørgensen <arne@arnested.dk>
2013-04-04 21:50:54 +0200

    Bumped version number.

--- drupal-spell.el
+++ drupal-spell.el
@@ -5,7 +5,7 @@
 ;; Author: Arne Jørgensen <arne@arnested.dk>
 ;; URL: https://github.com/arnested/drupal-spell
 ;; Created: March 22, 2013
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Keywords: wp
 
 ;; This program is free software; you can redistribute it and/or modify
